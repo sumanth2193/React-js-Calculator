38707f9f7fa7fa3f74853048bb3ecf9d177ad4ad
sumanth2193 <83970797+sumanth2193@users.noreply.github.com>
2021-05-19 21:40:12 +0530

    Add files via upload
    Simple Calculator using React-js

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from "./components/KeyPadComponent";
+
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var checkResult = ''
+        if(this.state.result.includes('--')){
+            checkResult = this.state.result.replace('--','+')
+        }
+
+        else {
+            checkResult = this.state.result
+        }
+
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(checkResult) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        const dark = () => {
+            var element = document.body;
+            element.classList.add("dark-mode");
+            element.classList.remove("light-mode")
+        }
+        const light = () => {
+            var element = document.body;
+            element.classList.add("light-mode");
+            element.classList.remove("dark-mode")
+        }
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Calculator</h1>
+                    <button style={{width: "50%"}} onClick={light}>Light</button><button style={{width: "50%"}} onClick={dark}>Dark</button>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
